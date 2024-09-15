1b4cf768e16821b581d4e26d7790b58ed5a70533
Arham-Iqbal <iarham924@gmail.com>
2024-09-15 14:29:51 +0530

    correcetd code

--- script.js
+++ script.js
@@ -1,42 +1,29 @@
-let input=document.querySelector(".data")
-let submit=document.querySelector(".sbm")
-let new_game=document.querySelector(".new")
-let result=document.querySelector(".res")
+let x;  
+let input = document.querySelector(".data");
+let submit = document.querySelector(".sbm");
+let new_game = document.querySelector(".new");
+let result = document.querySelector(".res");
 
-submit.addEventListener("click",()=>{
-
-    if(x==input.value)
-    {
-        result.textContent="Correct"
-        console.log("correct")
-        input.value="";
-        reset()
-       
-
-    }
-    else
-    {  
-        result.textContent="Wrong"
-        console.log("Wrong")
-        input.value=""
-      
-    }
-})
-
-
-new_game.addEventListener("click",()=>{
-     x=Math.floor(Math.random()*100 +1 )
-    console.log(x)
-    result.textContent=""
-    input.value=""
-});
-
-// get triggers when number will be correct
-function reset(){
-    x=Math.floor(Math.random()*100 +1 )
-    console.log(x)
-    result.textContent=""
-    input.value=""
-
+function reset() {
+    x = Math.floor(Math.random() * 100 + 1);
+    console.log(x);
+    result.textContent = "";
+    input.value = "";
 }
 
+submit.addEventListener("click", () => {
+    if (x == input.value) {
+        result.textContent = "Correct";
+        console.log("correct");
+        input.value = "";
+        // reset();
+    } else {
+        result.textContent = "Wrong";
+        console.log("Wrong");
+        input.value = "";
+    }
+});
+
+new_game.addEventListener("click", () => {
+    reset();
+});
